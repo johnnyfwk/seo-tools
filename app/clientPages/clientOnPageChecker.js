340a4177d46c9fc421c38be03bc5c7560cb2aba4
Johnny Fong <johnnyfong@live.com>
2025-10-15 21:28:38 +0100

    On-page checker: fixing open graph front end.

--- app/clientPages/clientOnPageChecker.js
+++ app/clientPages/clientOnPageChecker.js
@@ -930,128 +930,84 @@ export default function ClientOnPageChecker() {
                 : <section id="on-page-checker-open-graph-tags">
                     <h2>Open Graph Tags</h2>
 
-                    {openGraphTags.missingTags.length ? (
-                        <div>
-                            <h3>Missing Tags:</h3>
-                            <ul>
-                                {openGraphTags.missingTags.map((tag, index) => (
-                                    <li key={index}>{tag}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    ) : null}
-
-                    {/* Only show table if any real OG tag (not empty or null) exists */}
-                    {[
-                        openGraphTags.siteName,
-                        openGraphTags.url,
-                        openGraphTags.isOgUrlIndexable,
-                        openGraphTags.ogUrlStatusCode,
-                        openGraphTags.ogUrlFinalUrl,
-                        openGraphTags.title,
-                        openGraphTags.description,
-                        openGraphTags.type,
-                        openGraphTags.image,
-                        openGraphTags.locale,
-                        openGraphTags.audio,
-                        openGraphTags.video,
-                        openGraphTags.determiner
-                    ].some(value => value && value !== '') && (
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th style={{ textAlign: 'left' }}>Tag</th>
-                                    <th style={{ textAlign: 'left' }}>Value</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {openGraphTags.siteName && (
-                                    <tr>
-                                        <td><strong>Site Name</strong></td>
-                                        <td>{openGraphTags.siteName}</td>
-                                    </tr>
-                                )}
-                                {openGraphTags.url && (
-                                    <tr>
-                                        <td><strong>URL</strong></td>
-                                        <td><Link href={openGraphTags.url} target="_blank">{openGraphTags.url}</Link></td>
-                                    </tr>
-                                )}
-                                {openGraphTags.isOgUrlIndexable !== '' && (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th style={{ textAlign: 'left' }}>Tag</th>
+                                <th style={{ textAlign: 'left' }}>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><strong>Site Name</strong></td>
+                                <td className={!openGraphTags.siteName ? "warning-background" : undefined}>{openGraphTags.siteName}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>URL</strong></td>
+                                <td className={!openGraphTags.url ? "warning-background" : undefined}>
+                                    <Link href={openGraphTags.url} target="_blank">{openGraphTags.url}</Link>
+                                </td>
+                            </tr>
+                            {openGraphTags.url
+                                ? <>
                                     <tr>
                                         <td><strong>Is URL Indexable?</strong></td>
-                                        <td>{openGraphTags.isOgUrlIndexable ? "Yes" : "No"}</td>
+                                        <td className={!openGraphTags.isOgUrlIndexable ? "error-background" : undefined}>{openGraphTags.isOgUrlIndexable ? "Yes" : "No"}</td>
                                     </tr>
-                                )}
-                                {openGraphTags.ogUrlStatusCode && (
                                     <tr>
                                         <td><strong>Status Code</strong></td>
-                                        <td>{openGraphTags.ogUrlStatusCode}</td>
+                                        <td className={openGraphTags.ogUrlStatusCode !== 200 ? "error-background" : undefined}>{openGraphTags.ogUrlStatusCode}</td>
                                     </tr>
-                                )}
-                                {openGraphTags.ogUrlFinalUrl && (
                                     <tr>
                                         <td><strong>Final URL</strong></td>
                                         <td><Link href={openGraphTags.ogUrlFinalUrl} target="_blank">{openGraphTags.ogUrlFinalUrl}</Link></td>
                                     </tr>
-                                )}
-                                {openGraphTags.title && (
-                                    <tr>
-                                        <td><strong>Title</strong></td>
-                                        <td>{openGraphTags.title}</td>
-                                    </tr>
-                                )}
-                                {openGraphTags.description && (
-                                    <tr>
-                                        <td><strong>Description</strong></td>
-                                        <td>{openGraphTags.description}</td>
-                                    </tr>
-                                )}
-                                {openGraphTags.type && (
-                                    <tr>
-                                        <td><strong>Type</strong></td>
-                                        <td>{openGraphTags.type}</td>
-                                    </tr>
-                                )}
-                                {openGraphTags.image && (
-                                    <tr>
-                                        <td><strong>Image</strong></td>
-                                        <td>
-                                            <img
-                                                src={openGraphTags.image || undefined}
-                                                alt="Open Graph Tag Image"
-                                                style={{ minWidth: "100px", maxWidth: "200px" }}
-                                            />
-                                        </td>
-                                    </tr>
-                                )}
-                                {openGraphTags.locale && (
-                                    <tr>
-                                        <td><strong>Locale</strong></td>
-                                        <td>{openGraphTags.locale}</td>
-                                    </tr>
-                                )}
-                                {openGraphTags.audio && (
-                                    <tr>
-                                        <td><strong>Audio</strong></td>
-                                        <td>{openGraphTags.audio}</td>
-                                    </tr>
-                                )}
-                                {openGraphTags.video && (
-                                    <tr>
-                                        <td><strong>Video</strong></td>
-                                        <td>{openGraphTags.video}</td>
-                                    </tr>
-                                )}
-                                {openGraphTags.determiner && (
-                                    <tr>
-                                        <td><strong>Determiner</strong></td>
-                                        <td>{openGraphTags.determiner}</td>
-                                    </tr>
-                                )}
-                            </tbody>
-                        </table>
-                    )}
+                                </>
+                                : null
+                            }
+                            <tr>
+                                <td><strong>Title</strong></td>
+                                <td className={!openGraphTags.title ? "warning-background" : undefined}>{openGraphTags.title}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Description</strong></td>
+                                <td className={!openGraphTags.description ? "warning-background" : undefined}>{openGraphTags.description}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Type</strong></td>
+                                <td className={!openGraphTags.type ? "warning-background" : undefined}>{openGraphTags.type}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Image</strong></td>
+                                <td  className={!openGraphTags.image ? "warning-background" : undefined}>
+                                    {openGraphTags.image
+                                        ? <img
+                                            src={openGraphTags.image || undefined}
+                                            alt="Open Graph Tag Image"
+                                            style={{ minWidth: "100px", maxWidth: "200px" }}
+                                        />
+                                        : null
+                                    }
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><strong>Locale</strong></td>
+                                <td className={!openGraphTags.locale ? "warning-background" : undefined}>{openGraphTags.locale}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Audio</strong></td>
+                                <td className={!openGraphTags.audio ? "warning-background" : undefined}>{openGraphTags.audio}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Video</strong></td>
+                                <td className={!openGraphTags.video ? "warning-background" : undefined}>{openGraphTags.video}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Determiner</strong></td>
+                                <td className={!openGraphTags.determiner ? "warning-background" : undefined}>{openGraphTags.determiner}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </section>
             }
         </>
