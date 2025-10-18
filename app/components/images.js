8ad7892dc41f919e8ce36cf6c89dba4ebaedd6e6
Johnny Fong <johnnyfong@live.com>
2025-10-18 19:12:11 +0100

    On-page checker: adding image status codes and final urls.

--- app/components/images.js
+++ app/components/images.js
@@ -14,6 +14,8 @@ export default function Images({ images }) {
                             <th style={{ textAlign: 'left' }}>Image Preview</th>
                             <th style={{ textAlign: 'left' }}>Alt Text</th>
                             <th style={{ textAlign: 'left' }}>Source URL</th>
+                            <th style={{ textAlign: 'center' }}>Status Code</th>
+                            <th style={{ textAlign: 'left' }}>Final URL</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -31,6 +33,8 @@ export default function Images({ images }) {
                                     </td>
                                     <td className={!image.alt ? "error-background" : undefined}>{image.alt}</td>
                                     <td><Link href={image.src} target="_blank">{image.src}</Link></td>
+                                    <td style={{ textAlign: 'center' }} className={image.statusCode !== 200 ? "error-background" : null}>{image.statusCode}</td>
+                                    <td style={{ textAlign: 'left' }}>{image.finalUrl}</td>
                                 </tr>
                             )
                         })}
