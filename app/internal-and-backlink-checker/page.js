6910ee01d5257b585563cef109f5ed7bd851594d
Johnny Fong <johnnyfong@live.com>
2025-12-03 10:48:52 +0000

    Fixing internal and backlink checker build error.

--- app/internal-and-backlink-checker/page.js
+++ app/internal-and-backlink-checker/page.js
@@ -1,24 +1,28 @@
-import ClientInternalAndBacklinkChecker from "../clientPages/clientInternalAndBacklinkChecker";
-import { getSlugFromFile } from "../lib/utils/utils";
-import { pages } from "@/data/pages";
+// import ClientInternalAndBacklinkChecker from "../clientPages/clientInternalAndBacklinkChecker";
+// import { getSlugFromFile } from "../lib/utils/utils";
+// import { pages } from "@/data/pages";
 
-const slug = getSlugFromFile(import.meta.url);
+// const slug = getSlugFromFile(import.meta.url);
 
-const page = pages.find((p) => p.slug === slug);
+// const page = pages.find((p) => p.slug === slug);
 
-export const metadata = {
-    title: page.titleTag,
-    description: page.metaDescription,
-    robots: {
-        index: false, //********************** Set to true when finished **********************//
-        follow: true,
-    }
-}
+// export const metadata = {
+//     title: page.titleTag,
+//     description: page.metaDescription,
+//     robots: {
+//         index: false, //********************** Set to true when finished **********************//
+//         follow: true,
+//     }
+// }
+
+// export default function InternalAndBacklinkChecker() {
+//     return (
+//         <ClientInternalAndBacklinkChecker
+//             metaDescription={page.metaDescription}
+//         />
+//     )
+// }
 
 export default function InternalAndBacklinkChecker() {
-    return (
-        <ClientInternalAndBacklinkChecker
-            metaDescription={page.metaDescription}
-        />
-    )
+    return <p>Page under construction.</p>
 }
