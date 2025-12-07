30ddac3f5cc00858e01eb47eb3c1fd7a2046649a
Johnny Fong <johnny.fong@hotmail.com>
2025-12-07 16:01:30 +0000

    Adding structured data to each tool/page.

--- app/image-alt-text-checker/page.js
+++ app/image-alt-text-checker/page.js
@@ -1,26 +1,21 @@
 import ClientSeoOnPageChecker from "../clientPages/clientSeoOnPageChecker";
 import { getSlugFromFile } from "../lib/utils/utils";
-import { pages } from "@/data/pages";
+import { siteUrl, siteName, pages } from "@/data/pages";
+import * as utils from '@/app/lib/utils/utils';
 
 const slug = getSlugFromFile(import.meta.url);
 
 const page = pages.find((p) => p.slug === slug);
 
-export const metadata = {
-    robots: {
-        index: page.robots.index,
-        follow: page.robots.follow,
-    },
-    alternates: {
-        canonical: page.canonicalUrl,
-    },
-    title: page.titleTag,
-    description: page.metaDescription,
-}
+if (!page) throw new Error(`Page not found: ${slug}`);
 
 const scrapeOptions = {
     all: false,
     images: true,
+}
+
+export function generateMetadata() {
+    return utils.generateMetadataForEachPage(siteUrl, siteName, page);
 }
 
 export default function ImageAltTextChecker() {
